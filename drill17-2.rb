7793c621a7c3884ae22e04ec499905d51254843e
takefumi24 <t_ohba24@yahoo.co.jp>
2020-06-22 10:50:51 +0900

    【drill17-2】gemの概念について

---
+++ drill17-2.rb
@@ -0,0 +1,9 @@
+# 以下のメソッドの中には、Rubyには元々存在しない、Railsとその周辺のライブラリが提供しているメソッドが含まれています。
+# どのようなサイトを参照すればよいかを考え、Rubyには含まれていないメソッドを答えてください。
+# またフィードバック用に、参照したサイトのURLを記入してください。
+
+"try,pluck,blank?,preence"
+
+
+"https://docs.ruby-lang.org/en/2.5.0/"
+"https://docs.ruby-lang.org/ja/search/"
