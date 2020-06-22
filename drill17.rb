9636dc068962842eda3130aa55f591cb82b81759
takefumi24 <t_ohba24@yahoo.co.jp>
2020-06-22 10:50:16 +0900

    【drill17】ハッシュの取扱

---
+++ drill17.rb
@@ -0,0 +1,16 @@
+# 1.以下のハッシュに対してメソッドを使用し、キーだけを1つの配列に入れてください。
+attr = {name: "田中", age: 27, height: 180, weight: 75}
+p attr.keys
+
+# 2.以下のハッシュに対してメソッドを使用し、値だけを1つの配列に入れてください。
+attr = {name: "田中", age: 27, height: 180, weight: 75}
+p attr.values
+
+# 3.以下のハッシュに対してメソッドを使用し、キーと値のペアの一覧を配列1に入れてください。
+attr = {name: "田中", age: 27, height: 180, weight: 75}
+p attr.to_a
+
+# 解答
+# 1.attr.keys
+# 2.attr.values
+# 3.attr.to_a
