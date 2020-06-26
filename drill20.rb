59d7d099353a12b6b343e515a827a35cf264dc90
takefumi24 <t_ohba24@yahoo.co.jp>
2020-06-26 10:13:50 +0900

    【drill20】ハッシュの取扱

---
+++ drill20.rb
@@ -0,0 +1,10 @@
+# 以下のハッシュから値だけを取り出し、配列にしてください。ただし、hashクラスのvaluesメソッドは利用しないこととします。
+attr = {name: "田中", age: 27, height: 180, weight: 75}
+values = []
+attr.each_value {|val| values << val}
+
+# 解答
+values = []
+attr.each do |key, value|
+  values << value
+end
