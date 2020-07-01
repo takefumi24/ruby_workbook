e34e635be94151f789933c6dc8fc7a6d81d26c98
takefumi24 <t_ohba24@yahoo.co.jp>
2020-07-01 09:26:01 +0900

    【drill23-2】to_iメソッドとpopメソッド

---
+++ drill23-2.rb
@@ -0,0 +1,13 @@
+#(1) 文字列を数字に変換するメソッドを答えなさい。また、そのメソッドを用いて、下記の変数strを文字列から数字に変換してください。
+str = "1"
+str = str.to_i
+p str
+
+#(2) 以下の配列arrayに対してあるメソッドを実行し、最後の値"something"を配列から削除してください。
+array = ["dog", "cat", "something"]
+array.pop
+p array
+
+# 解答
+# (1)str.to_i
+# (2)array.pop
