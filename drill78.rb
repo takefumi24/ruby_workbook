888f4ed727d4fa42441f89189c063deb9377e1a9
takefumi24 <t_ohba24@yahoo.co.jp>
2020-07-10 18:07:45 +0900

    【drill78】divmodメソッド

---
+++ drill78.rb
@@ -0,0 +1,11 @@
+# 14を3で割った商と余りを配列で返して下さい。
+# その後、「商 = ○, 余り = ○」となるように解答して下さい
+
+p 14.divmod(3)
+quotient,remainder = 14.divmod(3)
+p "商 = #{quotient},余り = #{remainder}"
+
+
+# 解答
+quotient,remainder = 14.divmod(3)
+p "商 = #{quotient},余り = #{remainder}"
