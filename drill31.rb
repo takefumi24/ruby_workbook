db9ba25f5caba07f6da5dcef35c87a1af068576b
takefumi24 <t_ohba24@yahoo.co.jp>
2020-07-13 10:52:22 +0900

    【drill31】timesメソッド

---
+++ drill31.rb
@@ -0,0 +1,15 @@
+# ユーザーに数字を入力してもらい、その数の回数だけHello!と表示させるコードを記述してください。
+
+puts "数字を入力して下さい"
+input = gets.to_i
+
+input.times{|i| puts "Hello!"}
+
+# 解答
+def output(num)
+  num.times{ puts "Hello!" }
+end
+
+puts "何回表示させますか？"
+num = gets.to_i
+output(num)
