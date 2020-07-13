c470eb3663956d693e1000f71346c66c339954ae
takefumi24 <t_ohba24@yahoo.co.jp>
2020-07-13 11:02:13 +0900

    【drill31-2】クラスとインスタンスを利用したRubyのプログラム作成

---
+++ drill31-2.rb
@@ -0,0 +1,39 @@
+# 以下の仕様にしたがってコードを記述してください。
+# ・Personクラスはプロパティ name, ageを持っている。
+# ・StudentクラスはPersonクラスを継承している。
+# ・Studentクラスにはintroduceメソッドが定義されている。実行すると
+# 　「私の名前は◯◯です。◯歳です」と表示がされる。
+# ・Studentクラスのインスタンスを作成し、introduceメソッドを実行する。
+
+class Person
+  def initialize(name,age)
+    @name = name
+    @age = age
+  end
+end
+
+class Student < Person
+  def introduce
+    p "私の名前は#{@name}です。#{@age}歳です。"
+  end
+end
+
+dan = Student.new("dan",31)
+dan.introduce
+
+# 解答
+class Person
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+end
+
+class Student < Person
+  def introduce
+    puts "私の名前は#{@name}です。#{@age}歳です"
+  end
+end
+
+yamada = Student.new("山田", 20)
+yamada.introduce
