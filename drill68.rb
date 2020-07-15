5d20583048b00820beb75bd73583aa82d0518dcc
takefumi24 <t_ohba24@yahoo.co.jp>
2020-07-15 11:46:22 +0900

    【drill68】ifと論理演算子

---
+++ drill68.rb
@@ -0,0 +1,24 @@
+# 数値numが1以上10以下の範囲であればTrueを出力します。
+# outside_modeがTrueであった場合は、数値が0以下、または11以上であってもTrueを出力します。
+# それ以外はFalseを出力するメソッドを作りましょう。
+
+def in1to10(num,outside_mode)
+  if num >= 1 && num <= 10 || outside_mode == true
+    p "True"
+  else
+    p "False"
+  end
+end
+
+in1to10(5,false)
+in1to10(11,false)
+in1to10(11,true)
+
+# 解答
+def in1to10(num,outside_mode)
+  if (num >= 1 && num <= 10) || outside_mode
+    puts "True"
+  else
+    puts "False"
+  end
+end
